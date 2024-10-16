ba4010824057bb09388b088540ffc1f18132bca7
Lengthorn, Olivia <olivia.lengthorn@bp.com>
2024-10-16 13:38:19 +0100

    Adding SelectionSort

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sorting_Algorithms;
+class Program
+{
+    public static void Main()
+    {
+        int[] array = { 75, 25, 1, 46, 11 };
+
+        Console.Write("Original array: ");
+        SelectionSort.PrintArray(array);
+
+        SelectionSort.Sort(array);
+
+        Console.Write("Sorted array: ");
+        SelectionSort.PrintArray(array);
+    }
+
+}
---
+++ SelectionSort.cs
@@ -0,0 +1,36 @@
+public static class SelectionSort
+{
+    public static void Sort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i)
+            {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
